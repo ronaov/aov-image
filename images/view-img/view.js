763c14e4ae1c12961c6ec3a6dd0ac7ffb8e75bb6
ron <ronlatoiday@gmail.com>
2025-11-30 16:59:59 +0700

    update

--- images/view-img/view.js
+++ images/view-img/view.js
@@ -2,9 +2,13 @@ const s = new URLSearchParams(window.location.search)
 var id = s.get('id')
 var link
 
-if (id === "None") link = "None.jpg"
-if (id === "warning") link = "warning.jpg"
-else link = "https://dl.ops.kgtw.garenanow.com/CHT/HeroTrainingLoadingNew_B36/" + id +".jpg"
+if (id === "None") {
+	link = "/images/view-img/None.jpg"
+} else if (id === "warning") {
+	link = "warning.jpg"
+} else {
+	link = "https://dl.ops.kgtw.garenanow.com/CHT/HeroTrainingLoadingNew_B36/" + id +".jpg"
+}
 
 var img = document.getElementById('img')
 img.src = link
